00d357bf62ffefae77a7c5c4d35a37e5d30eb8c9
byunjiin <jibyun5808@naver.com>
2024-02-03 16:55:27 +0900

    2주차

---
+++ 2week/4.Git/2_MathTool/calculator.py
@@ -0,0 +1,13 @@
+#기본 계산기
+def add(a, b):
+  return a+b
+def subtract(a, b):
+  return a-b
+def multiply(a, b):
+<<<<<<< HEAD
+  return a*b
+=======
+  return a*b
+def divide(a, b):
+  return a/b
+>>>>>>> master
