7bd126999dcb119d533255d93abf8c70669f67aa
byunjiin <jibyun5808@naver.com>
2024-02-03 22:57:13 +0900

    이미지다운

--- 6week/1.React_Start/1_dicegame/src/index.js
+++ 6week/1.React_Start/1_dicegame/src/index.js
@@ -1,6 +1,4 @@
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(
-  <h1>안녕 리액트!</h1>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
