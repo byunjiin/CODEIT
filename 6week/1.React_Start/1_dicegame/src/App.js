19c6c39c10d42faca02aa509e2f7546bc214aeba
byunjiin <jibyun5808@naver.com>
2024-02-09 09:46:14 +0900

    주사위 state활용

--- 6week/1.React_Start/1_dicegame/src/App.js
+++ 6week/1.React_Start/1_dicegame/src/App.js
@@ -1,9 +1,37 @@
-import Dice from './Dice';
+import Board from './Board';
+import Button from './Button';
+import { useState } from 'react';
 
-function App() {
+function random(n){
+  return Math.ceil(Math.random() * n); // 올림
+}
+
+function App(){
+  const [myHistory, setMyHistory] = useState([]);
+  const [otherHistory, setOtherHistory] = useState([]);
+
+  const handleRollClick = () => {
+    const nextMyNum = random(6);
+    const nextOtherNum = random(6);
+    setMyHistory([...myHistory, nextMyNum]); // state를 수정하는 방법
+    setOtherHistory([...otherHistory, nextOtherNum]); // state를 수정하는 방법
+  };
+
+  const handleClearClick = () => {
+    setMyHistory([]); // 빈배열
+    setOtherHistory([]); // 빈배열
+  };
+
   return (
     <div>
-      <Dice />
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <div>
+        <Board name="나" color="blue" gameHistory={myHistory}/>
+        <Board name="상대" color="red" gameHistory={otherHistory}/>
+      </div>
     </div>
   );
 }
