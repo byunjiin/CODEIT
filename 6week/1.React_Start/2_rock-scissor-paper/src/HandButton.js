ff3b0d8e62ddab6a63349c8a90d9147560c3f548
byunjiin <jibyun5808@naver.com>
2024-02-09 20:07:37 +0900

    가위바위보 hand버튼 인라인 css

--- 6week/1.React_Start/2_rock-scissor-paper/src/HandButton.js
+++ 6week/1.React_Start/2_rock-scissor-paper/src/HandButton.js
@@ -1,9 +1,23 @@
 import HandIcon from './HandIcon';
 
+const style = { //주의:속성명은 카멜표기법
+  width: '166px',
+  height: '166px',
+  border: 'none',
+  outline: 'none',
+  textAlign: 'center',
+  cursor: 'pointer',
+  backgroundColor: 'transparent',
+  backgroundImage: `url('./assets/purple.svg')`,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: 'contain',
+};
+
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
   return (
-    <button onClick={handleClick}>
+    <button style={style} onClick={handleClick}>
       <HandIcon value={value} />
     </button>
   );
