e61c57fc5e2d7f21632b902aedea49700613dcd3
byunjiin <jibyun5808@naver.com>
2024-02-10 19:40:14 +0900

    가위바위보 버튼 스타일

--- 6week/1.React_Start/2_rock-scissor-paper/src/HandButton.js
+++ 6week/1.React_Start/2_rock-scissor-paper/src/HandButton.js
@@ -1,24 +1,11 @@
 import HandIcon from './HandIcon';
-
-const style = { //주의:속성명은 카멜표기법
-  width: '166px',
-  height: '166px',
-  border: 'none',
-  outline: 'none',
-  textAlign: 'center',
-  cursor: 'pointer',
-  backgroundColor: 'transparent',
-  backgroundImage: `url('./assets/purple.svg')`,
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  backgroundSize: 'contain',
-};
+import './HandButton.css';
 
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
   return (
-    <button style={style} onClick={handleClick}>
-      <HandIcon value={value} />
+    <button className='HandButton' onClick={handleClick}>
+      <HandIcon className='HandButton-icon' value={value} />
     </button>
   );
 }
--- 6week/1.React_Start/2_rock-scissor-paper/src/HandIcon.js
+++ 6week/1.React_Start/2_rock-scissor-paper/src/HandIcon.js
@@ -2,15 +2,15 @@ import rockImg from './assets/rock.svg';
 import scissorImg from './assets/scissor.svg';
 import paperImg from './assets/paper.svg';
 
-const IMAGES ={
+const IMAGES = {
   rock: rockImg,
   scissor: scissorImg,
   paper: paperImg,
 };
 
-function HandIcon({value}) {
+function HandIcon({ className, value }) {
   const src = IMAGES[value];
-  return <img src={src} alt={value} />;
+  return <img className={className} src={src} alt={value} />;
 }
 
 export default HandIcon;
